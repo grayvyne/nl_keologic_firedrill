77c5f9f01c43670922600abbaf228ac6f9858c7e
Brian Weyer <brianweyer@gmail.com>
2018-04-11 09:28:07 -0500

    Initializing index for react-md

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import { AppRegistry } from 'react-native';
+import * as WebFont from 'webfontloader';
 
-// ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+WebFont.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons'],
+  },
+});
+
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 
 AppRegistry.registerComponent('App', () => App);
 AppRegistry.runApplication('App', { rootTag: document.getElementById('root') });
