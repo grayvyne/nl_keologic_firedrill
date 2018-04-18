cd4255ec0ef82c2bd4c1f0f92c2ed34787a58357
Brian Weyer <brianweyer@gmail.com>
2018-04-18 13:53:15 -0500

    Set up ClassesTableCell to follow React Native structure and elements

--- src/components/navigators/ClassesTableCell.tsx
+++ src/components/navigators/ClassesTableCell.tsx
@@ -1,8 +1,54 @@
 import * as React from 'react';
-import { TableRow, TableCell, Button } from 'material-ui';
 import RightArrow from '../../assets/RightArrow';
 import { SingleClass } from '../pages/Classes';
 import teal from 'material-ui/colors/teal';
+import { View, Text, TouchableOpacity, ViewStyle, TextStyle } from 'react-native';
+
+namespace style {
+    export const cellContainer: ViewStyle = { padding: 0, height: 'unset' };
+    export const touchableHighlight: ViewStyle = {
+        width: '100%',
+        height: '100%',
+        padding: 0
+    };
+    export const highlightWrapper: ViewStyle = {
+        display: 'flex',
+        flexGrow: 1,
+        flexDirection: 'row',
+        padding: 20,
+        alignItems: 'center'
+    };
+    export const labelContainer: ViewStyle = {
+        display: 'flex',
+        flexGrow: 1
+    };
+    export const label: ViewStyle = {
+        marginBottom: 3
+    };
+    export const labelText: TextStyle = {
+        textAlign: 'left',
+        fontWeight: '400',
+        fontSize: 16
+    };
+    export const subLabelText: TextStyle = {
+        textAlign: 'left',
+        fontWeight: '300',
+        color: 'grey'
+    };
+    export const classCountText: TextStyle = {
+        paddingLeft: 50,
+        paddingRight: 20,
+        color: teal[400],
+        fontSize: 24,
+        fontWeight: '300'
+    };
+    export const rightArrowContainer: ViewStyle = {
+        height: 10,
+        width: 6,
+        marginRight: 0,
+        marginTop: -4
+    };
+}
 
 interface ClassesTableCellProps {
     singleClass: SingleClass;
@@ -13,64 +59,29 @@ export default class ClassesTableCell extends React.Component<ClassesTableCellPr
     singleClass = this.props.singleClass;
 
     render() {
+        const { singleClass } = this.props;
         return (
-            <TableRow key={this.singleClass.id} style={{ border: 'none', padding: 0, height: 'unset' }}>
-                <TableCell
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                        padding: 0,
-                        border: 'unset'
-                    }}
-                >
-                    <Button onClick={this.props.onClick} style={{ width: '100%', height: '100%', padding: 0 }}>
-                        <div
-                            style={{
-                                display: 'flex',
-                                flexGrow: 1,
-                                flexDirection: 'column',
-                                padding: 20
-                            }}
-                        >
-                            <div
-                                style={{
-                                    textAlign: 'left',
-                                    fontWeight: 400,
-                                    textTransform: 'capitalize',
-                                    fontSize: 16,
-                                    marginBottom: '3px'
-                                }}
-                            >
-                                {this.singleClass.name}
-                            </div>
-                            <div
-                                style={{
-                                    textAlign: 'left',
-                                    fontWeight: 300,
-                                    textTransform: 'capitalize',
-                                    color: 'grey'
-                                }}
-                            >
-                                Grade {this.singleClass.grade}
-                            </div>
-                        </div>
-                        <div
-                            style={{
-                                paddingLeft: 50,
-                                paddingRight: 20,
-                                color: teal[400],
-                                fontSize: 24,
-                                fontWeight: 300
-                            }}
-                        >
-                            {this.singleClass.found}/{this.singleClass.total}
-                        </div>
-                        <div style={{ height: 10, width: 6, marginRight: 20, marginTop: -4 }}>
+            <View key={singleClass.id} style={style.cellContainer}>
+                <TouchableOpacity onPress={this.props.onClick} style={style.touchableHighlight}>
+                    <View style={style.highlightWrapper}>
+                        <View style={style.labelContainer}>
+                            <View style={style.label}>
+                                <Text style={style.labelText}>{singleClass.name}</Text>
+                            </View>
+                            <View>
+                                <Text style={style.subLabelText}>Grade {singleClass.grade}</Text>
+                            </View>
+                        </View>
+
+                        <Text style={style.classCountText}>
+                            {singleClass.found}/{singleClass.total}
+                        </Text>
+                        <View style={style.rightArrowContainer}>
                             <RightArrow height={10} width={6} />
-                        </div>
-                    </Button>
-                </TableCell>
-            </TableRow>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            </View>
         );
     }
 }
