d039a40014baf73a57a412080b9d9dc0609584fa
Brian Weyer <brianweyer@gmail.com>
2018-04-18 13:56:46 -0500

    Update view to incorporate updated content views

--- src/components/pages/Classes.tsx
+++ src/components/pages/Classes.tsx
@@ -1,13 +1,16 @@
 import AppsIcon from '@material-ui/icons/Apps';
 import CheckIcon from '@material-ui/icons/CheckCircle';
-import { Badge, IconButton, Toolbar, Table, TableBody } from 'material-ui';
+import { Badge, IconButton, Toolbar } from 'material-ui';
 import AppBar from 'material-ui/AppBar';
 import Tabs, { Tab } from 'material-ui/Tabs';
 import * as React from 'react';
 import SwipeableViews from 'react-swipeable-views';
 import TabStyles from '../../config/TabStyles';
 import ClassesTableCell from '../navigators/ClassesTableCell';
-import PeopleIcon from '@material-ui/icons/People';
+import ActionTableCell from '../shared/ActionTableCell';
+import ContentView from '../shared/ContentView';
+import ScrollView from '../shared/ScrollView';
+import TableView from '../shared/TableView';
 
 export type SingleClass = {
     id: number;
@@ -26,11 +29,6 @@ interface ClassesProps {
 }
 
 export class Classes extends React.Component<ClassesProps, ClassesState> {
-    static navigationOptions = {
-        tabBarIcon: (focused: boolean, tintColor: string) => {
-            return <PeopleIcon />;
-        }
-    };
     data: SingleClass[] = [
         {
             id: 1,
@@ -216,56 +214,60 @@ export class Classes extends React.Component<ClassesProps, ClassesState> {
         };
     }
 
-    handleChange = (event: any, index: any) => {
+    handleTabChange = (event: any, index: any) => {
+        this.setState({ index });
+    };
+
+    handleChange = (index: any) => {
         this.setState({ index });
     };
 
     render() {
         return true ? (
             <div>
-                <AppBar position={'fixed'} style={{ boxShadow: 'none' }}>
-                    <Toolbar style={{ alignItems: 'stretch' }}>
-                        <IconButton color="inherit" aria-label="Menu" style={{ alignSelf: 'center', marginLeft: -10 }}>
-                            <AppsIcon />
-                        </IconButton>
-                        <Tabs
-                            value={this.state.index}
-                            onChange={this.handleChange}
-                            indicatorColor="white"
-                            textColor="inherit"
-                            fullWidth={true}
-                            style={{ height: '100%' }}
-                        >
-                            <Tab label={<span style={{ fontSize: 10 }}>Your Classes</span>} style={TabStyles} />
-                            <Tab label={<span style={{ fontSize: 10 }}>Find Classes</span>} style={TabStyles} />
-                            <Tab
-                                label={
-                                    <span>
-                                        <span style={{ fontSize: 10, marginRight: 40 }}>Unclaimed</span>
-                                        <Badge
-                                            color="secondary"
-                                            badgeContent={999}
-                                            children={<span />}
-                                            style={{ marginLeft: -20, fontSize: 8 }}
-                                        />
-                                    </span>
-                                    // tslint:disable-next-line:jsx-curly-spacing
-                                }
-                                style={TabStyles}
+                <ContentView>
+                    <AppBar position={'fixed'} style={{ boxShadow: 'none' }}>
+                        <Toolbar style={{ alignItems: 'stretch' }}>
+                            <IconButton
+                                color="inherit"
+                                aria-label="Menu"
+                                style={{ alignSelf: 'center', marginLeft: -10 }}
                             >
-                                <CheckIcon />
-                            </Tab>
-                        </Tabs>
-                    </Toolbar>
-                </AppBar>
-                <SwipeableViews
-                    index={this.state.index}
-                    onChangeIndex={this.handleChange}
-                    style={{ display: 'flex', flexGrow: 1, alignSelf: 'stretch' }}
-                >
-                    <div>
-                        <Table>
-                            <TableBody>
+                                <AppsIcon />
+                            </IconButton>
+                            <Tabs
+                                value={this.state.index}
+                                onChange={this.handleTabChange}
+                                indicatorColor="white"
+                                textColor="inherit"
+                                fullWidth={true}
+                                style={{ height: '100%' }}
+                            >
+                                <Tab label={<span style={{ fontSize: 10 }}>Your Classes</span>} style={TabStyles} />
+                                <Tab label={<span style={{ fontSize: 10 }}>Find Classes</span>} style={TabStyles} />
+                                <Tab
+                                    label={
+                                        <span>
+                                            <span style={{ fontSize: 10, marginRight: 40 }}>Unclaimed</span>
+                                            <Badge
+                                                color="secondary"
+                                                badgeContent={999}
+                                                children={<span />}
+                                                style={{ marginLeft: -20, fontSize: 8 }}
+                                            />
+                                        </span>
+                                        // tslint:disable-next-line:jsx-curly-spacing
+                                    }
+                                    style={TabStyles}
+                                >
+                                    <CheckIcon />
+                                </Tab>
+                            </Tabs>
+                        </Toolbar>
+                    </AppBar>
+                    <SwipeableViews index={this.state.index} onChangeIndex={this.handleChange}>
+                        <ScrollView>
+                            <TableView>
                                 {this.data.map(singleClass => {
                                     return (
                                         <ClassesTableCell
@@ -275,12 +277,31 @@ export class Classes extends React.Component<ClassesProps, ClassesState> {
                                         />
                                     );
                                 })}
-                            </TableBody>
-                        </Table>
-                    </div>
-                    <div>Test Two</div>
-                    <div>Test Three</div>
-                </SwipeableViews>
+                            </TableView>
+                        </ScrollView>
+                        <ScrollView>
+                            <TableView>
+                                {this.data.map(singleClass => {
+                                    return (
+                                        <ActionTableCell
+                                            onClick={() => (this.props as any).navigation.navigate('ClassDetail')}
+                                            cellData={{
+                                                id: singleClass.id,
+                                                label: singleClass.name,
+                                                subLabel: singleClass.grade
+                                            }}
+                                            key={singleClass.id}
+                                            buttonLabel={'Claim'}
+                                            buttonColor={'red'}
+                                            buttonTextColor={'white'}
+                                        />
+                                    );
+                                })}
+                            </TableView>
+                        </ScrollView>
+                        <div>Test Three</div>
+                    </SwipeableViews>
+                </ContentView>
             </div>
         ) : null;
     }
