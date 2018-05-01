d98a9e3f8f03d5b70fcc38eceb98efd736a74ac7
Drew Wiskus <drew.wiskus@newlionlabs.com>
2018-05-01 12:07:41 -0500

    Auto stash before revert of "added firebase calls to search student tableview cells"

--- src/components/pages/Search.tsx
+++ src/components/pages/Search.tsx
@@ -11,15 +11,25 @@ import { Student } from '../../models/Student';
 import { Stores } from '../../stores';
 import { ApplicationServices } from '../../services/ApplicationServices';
 import { ContentView, SearchBar, StudentTableCell, TableView, AppBar } from '../shared';
+import { SearchTabStrings as ui } from '../../config/uiConstants';
+import { Status } from '../../models/Status';
+import { ChangeEvent } from 'react';
+import { MaterialRadioInputList } from '../shared/PopupModals/MaterialRadioInputList';
 
 interface State {
     index: number;
+    editStatusModalIsVisible: boolean;
+    selectedStudentStatus: Status;
+    selectedStudent?: Student;
 }
 
 interface Props {
     students: Student[];
     searchTerm: string;
     onChangeSearchTerm(term: string): void;
+    markStudentAbsent(studentID: number): void;
+    markStudentMissing(studentID: number): void;
+    markStudentFound(studentID: number): void;
 }
 
 namespace styles {
@@ -46,7 +56,9 @@ class Search extends React.Component<Props, State> {
     public constructor(props: Props) {
         super(props);
         this.state = {
-            index: 0
+            index: 0,
+            editStatusModalIsVisible: false,
+            selectedStudentStatus: Status.Found
         };
     }
 
@@ -74,9 +86,65 @@ class Search extends React.Component<Props, State> {
                         <TableView>{this.props.students.map(this.renderTableCell)}</TableView>
                     </ScrollView>
                 </ContentView>
+
+                <MaterialRadioInputList
+                    open={this.state.editStatusModalIsVisible}
+                    modalHeader={ui.CHOOSE_STATUS}
+                    currentlySelectedRadioOptionValue={this.state.selectedStudentStatus}
+                    radioOptions={[Status.Found, Status.Absent, Status.Missing]}
+                    onPressRadioOption={this.onPressRadioOption}
+                    onPressCancel={() => this.cancelUpdateStudentStatus()}
+                    onPressAffirm={() => this.updateStudentStatus()}
+                    cancelButtonLabel={ui.CANCEL}
+                    affirmButtonLabel={ui.OK}
+                />
             </View>
         );
     }
+
+    private onPressRadioOption = (event: ChangeEvent<HTMLInputElement>) => {
+        const valueString = event.target.value;
+        const status = Status[valueString];
+        console.assert(
+            status !== undefined,
+            `event.target.value: "${valueString}" from @onPressRadioOption() in #ClassDetail.tsx doesn't match the Status enum and returned undefined`
+        );
+
+        this.setState({ selectedStudentStatus: status });
+    };
+
+    private cancelUpdateStudentStatus = () => {
+        this.setState({ editStatusModalIsVisible: false });
+    };
+
+    private updateStudentStatus = () => {
+        const updatedStatus = this.state.selectedStudentStatus;
+        const selectedStudent = this.state.selectedStudent!;
+
+        this.markStudentAs(updatedStatus, selectedStudent);
+
+        this.setState({
+            editStatusModalIsVisible: false,
+            selectedStudentStatus: Status.Found,
+            selectedStudent: undefined
+        });
+    };
+
+    private markStudentAs = (status: Status, student: Student) => {
+        switch (status) {
+            case Status.Missing:
+                this.props.markStudentMissing(student.userID);
+                break;
+            case Status.Found:
+                this.props.markStudentFound(student.userID);
+                break;
+            case Status.Absent:
+                this.props.markStudentAbsent(student.userID);
+                break;
+            default:
+                throw new Error('Case unaccounted for @updateStudentStatus #ClassDetail.tsx');
+        }
+    };
 
     private renderTableCell = (student: Student): JSX.Element => {
         return (
@@ -84,18 +152,25 @@ class Search extends React.Component<Props, State> {
                 student={student}
                 status={student.status}
                 onClick={() => {
-                    return;
+                    this.openUpdateStatusModal(student);
                 }}
             />
         );
     };
+
+    private openUpdateStatusModal(student: Student) {
+        this.setState({ selectedStudent: student, editStatusModalIsVisible: true });
+    }
 }
 
 function mapStoresToProps({ firedrillStore }: Stores): Props {
     return {
         students: firedrillStore.matchingSearchStudents,
         searchTerm: firedrillStore.studentSearchTerm,
-        onChangeSearchTerm: term => firedrillStore.setStudentSearchTerm(term)
+        onChangeSearchTerm: term => firedrillStore.setStudentSearchTerm(term),
+        markStudentAbsent: (id: number) => firedrillStore.markStudentAsAbsent(id),
+        markStudentMissing: (id: number) => firedrillStore.markStudentAsMissiong(id),
+        markStudentFound: (id: number) => firedrillStore.markStudentAsFound(id)
     };
 }
 
